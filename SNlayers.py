3c4c2b3e8ee7de6726f1722447282ceb478a7da9
AntixK <anandkrish894@gmail.com>
2019-03-05 13:03:56 +0900

    Updated SN layers

--- SNlayers.py
+++ SNlayers.py
@@ -84,3 +84,26 @@ class MeanSpectralNorm(nn.BatchNorm2d):
 
         y = self.weight.view(-1, 1) * y + self.bias.view(-1, 1)
         return y.view(return_shape).transpose(0,1)
+
+# Alternate Definition of MSN (Doen't seem to work properly)
+# class MeanSpectralNorm(nn.BatchNorm2d):
+#     def forward(self, input):
+#         self._check_input_dim(input)
+
+#         exponential_average_factor = 0.0
+#         #self.register_parameter('weight', None)
+#         self.running_var = self.running_var*1.0
+
+#         if self.training and self.track_running_stats:
+#             # TODO: if statement only here to tell the jit to skip emitting this when it is None
+#             if self.num_batches_tracked is not None:
+#                 self.num_batches_tracked += 1
+#                 if self.momentum is None:  # use cumulative moving average
+#                     exponential_average_factor = 1.0 / float(self.num_batches_tracked)
+#                 else:  # use exponential moving average
+#                     exponential_average_factor = self.momentum
+
+#         return F.batch_norm(
+#             input, self.running_mean, self.running_var, self.weight*0.0, self.bias,
+#             self.training or not self.track_running_stats,
+#             exponential_average_factor, self.eps)
